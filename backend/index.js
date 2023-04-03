673b257bbf42765273b7b4c7239e19ae77f472cb
Siddharth Kumar <siddharthkumar726@gmail.com>
2023-04-03 19:02:47 +0530

    Added Post request handlers

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,12 @@ const port = 3000
 
 connectToMongo();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.use(express.json())
+
+//Available Routes
+app.use('/api/auth',require("./routes/auth"))
+app.use('/api/notes',require("./routes/notes"))
+
   
   app.listen(port, () => {
     console.log(`Example app listening on port  http://localhost:${port}`)
